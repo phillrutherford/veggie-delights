498af4f0bf834e8cd05263891c590f8f14fa667f
phillrutherford <phillip.h.rutherford@gmail.com>
2020-12-07 18:04:44 +0000

    debug and correct app.py and tasks.html

--- app.py
+++ app.py
@@ -14,14 +14,17 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
+print(app.config["MONGO_URI"])
+
 mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
-    tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks)
+@app.route("/get_meals")
+def get_meals():
+    meals = mongo.db.meals.find()
+    return render_template("tasks.html", meals=meals)
 
 
 if __name__ == "__main__":
